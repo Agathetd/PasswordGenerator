57fb2c69d38205154327081d9939d012fe2601a4
Agathetd <adrienduriez3@hotmail.fr>
2023-04-09 13:14:26 +0200

    first commit

---
+++ passwordgenerator.java
@@ -0,0 +1,11 @@
+public class passwordgenerator {
+
+    String chars = "AZERTYUIOPQSDFGHJKLMWXCVBN?./§¨%£µ1234567890°+<azertyuiop^$*qsdfghjklmùwxcvbn,;:!";
+    public String password;
+
+    public String generatePassword() {
+        
+        
+        return password;   
+    }
+}
