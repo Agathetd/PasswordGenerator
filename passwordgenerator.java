cc761e35ec111872a4f9d02a76f28fa9bc66a0a7
Agathetd <adrienduriez3@hotmail.fr>
2023-04-09 13:34:33 +0200

    code fonctionnel

--- passwordgenerator.java
+++ passwordgenerator.java
@@ -1,11 +1,25 @@
+import java.util.Random;
+
 public class passwordgenerator {
 
-    String chars = "AZERTYUIOPQSDFGHJKLMWXCVBN?./§¨%£µ1234567890°+<azertyuiop^$*qsdfghjklmùwxcvbn,;:!";
-    public String password;
+    static String chars = "AZERTYUIOPQSDFGHJKLMWXCVBN?./§¨%£µ1234567890°+<azertyuiop^$*qsdfghjklmùwxcvbn,;:!";
 
-    public String generatePassword() {
-        
-        
-        return password;   
+    public static String generatePassword() {
+        int charsLength = chars.length(); 
+
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 15; i++) {
+            int index = random.nextInt(charsLength);
+            sb.append(chars.charAt(index));
+        }
+        return sb.toString();
     }
+
+    public static void main(String[] args) {
+        String password = generatePassword();
+        System.out.println("Mot de passe généré :" + password);
+
+    }
+
 }
